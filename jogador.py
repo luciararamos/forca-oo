c05550e8ceabcadd25871fbeded70c77a9684d36
Luciara Zuleide Alves Merencio Ramos <lzamr@cesar-ldap.intranet>
2025-03-27 19:08:47 -0300

    python oo

---
+++ jogador.py
@@ -0,0 +1,12 @@
+class Jogador:   #dados do jogador
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def __str__(self):
+        return self.nome
+    
+    def __str__(self):
+        return self.idade
+    
